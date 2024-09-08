d6ad6adb490e9f56fed62e33aae99246bdf9d854
charlenefialho <fialhocharlene@gmail.com>
2024-09-08 13:01:58 -0300

    fix: change path clothes images

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,14 @@
 -- Peca de Roupa 1
-INSERT INTO clothing_item ( nome, categoria, cor, tamanho, imagem_url, material, marca, padrao, preco, data_cadastro, sazonalidade) VALUES ( 'Camiseta Branca', 'Camiseta', 'Branco', 'M', 'https://example.com/camiseta-branca.jpg', 'Algodão', 'Marca A', 'Liso', 49.90, '2024-09-01', 'Verão');
+INSERT INTO clothing_item ( nome, categoria, cor, tamanho, imagem_url, material, marca, padrao, preco, data_cadastro, sazonalidade) VALUES ( 'Camiseta Branca', 'Camiseta', 'Branco', 'M', 'https://abrir.link/DwOYl', 'Algodão', 'Marca A', 'Liso', 49.90, '2024-09-01', 'Verão');
 
 -- Peca de Roupa 2
-INSERT INTO clothing_item (nome, categoria, cor, tamanho, imagem_url, material, marca, padrao, preco, data_cadastro, sazonalidade) VALUES ('Vestido Floral', 'Vestido', 'Azul', 'P', 'https://example.com/vestido-floral.jpg', 'Poliéster', 'Marca B', 'Floral', 129.90, '2024-08-15', 'Primavera');
+INSERT INTO clothing_item (nome, categoria, cor, tamanho, imagem_url, material, marca, padrao, preco, data_cadastro, sazonalidade) VALUES ('Vestido Floral', 'Vestido', 'Azul', 'P', 'https://abrir.link/xmZnn', 'Poliéster', 'Marca B', 'Floral', 129.90, '2024-08-15', 'Primavera');
 
 -- Peca de Roupa 3
-INSERT INTO clothing_item (nome, categoria, cor, tamanho, imagem_url, material, marca, padrao, preco, data_cadastro, sazonalidade) VALUES ('Calça Jeans', 'Calça', 'Azul', 'G', 'https://example.com/calca-jeans.jpg', 'Jeans', 'Marca C', 'Liso', 99.90, '2024-07-10', 'Outono');
+INSERT INTO clothing_item (nome, categoria, cor, tamanho, imagem_url, material, marca, padrao, preco, data_cadastro, sazonalidade) VALUES ('Calça Jeans', 'Calça', 'Azul', 'G', 'https://abrir.link/AnnAE', 'Jeans', 'Marca C', 'Liso', 99.90, '2024-07-10', 'Outono');
 
 -- Peca de Roupa 4
-INSERT INTO clothing_item (nome, categoria, cor, tamanho, imagem_url, material, marca, padrao, preco, data_cadastro, sazonalidade) VALUES ('Jaqueta de Couro', 'Jaqueta', 'Preto', 'M', 'https://example.com/jaqueta-couro.jpg', 'Couro', 'Marca D', 'Liso', 299.90, '2024-09-05', 'Inverno');
+INSERT INTO clothing_item (nome, categoria, cor, tamanho, imagem_url, material, marca, padrao, preco, data_cadastro, sazonalidade) VALUES ('Jaqueta de Couro', 'Jaqueta', 'Preto', 'M', 'https://abrir.link/cFHDB', 'Couro', 'Marca D', 'Liso', 299.90, '2024-09-05', 'Inverno');
 
 
 --Lookbook 1
